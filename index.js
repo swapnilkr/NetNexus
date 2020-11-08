0dff4e683f77782debd115b81bd6831ea0e473c4
Swapnil <swapnilkr099@gmail.com>
2020-11-08 21:21:51 +0530

    First commit create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+
+const app=express();
+
+const port=8000;
+
+
+
+app.listen (port,function(err)
+{
+    if (err)
+    {
+        // console.log('Error',err);
+
+        console.log(`Error in running the server :${err}`);
+    }
+    console.log(`Server running on port :${port}`);
+
+
+});
