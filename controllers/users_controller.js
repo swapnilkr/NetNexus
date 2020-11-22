25753d4a3c6bfb8630ed1fd7ef091bedec557b5b
Swapnil <swapnilkr099@gmail.com>
2020-11-22 14:06:01 +0530

    Added Signup Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+// require user
+const User=require('../models/user');
+
 module.exports.profile = function(req,res)
 {
     return res.render('user_profile',{
@@ -23,7 +26,40 @@ module.exports.signIn=function(req,res){
 
 // get the sign up data
 module.exports.create=function(req,res){
-    //todo later
+    // if password and confirm password are different
+    if(req.body.password!= req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }
+    // if password and confirm password are same
+    // we find if the email is already present or not in db
+
+    // we use find function to find data from user schema
+    User.findOne({email : req.body.email},function(err,user)
+    {
+        if(err)
+        {
+            console.log('error in finding user in signing up');
+            return;
+        }
+
+        if(!user)
+        {
+            User.create(req.body,function(err,user)
+            {
+                console.log('error in creating user while signing up');
+                // after creating redirect to sign up page
+                return res.redirect('/users/sign-in');
+            });
+            
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+
+    });
+
 }
 
 // sign in and create a session for the user
--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,6 @@ router.get('/sign-up',usersController.signUp);
 
 router.get('/sign-in',usersController.signIn);
 
+router.post('/create',usersController.create);
+
 module.exports=router;
