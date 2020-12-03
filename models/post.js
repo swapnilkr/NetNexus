b175ffa0716779fdd0512a3218a3dcacc61c198b
Swapnil <swapnilkr099@gmail.com>
2020-12-03 20:14:22 +0530

    Added Feed Post

--- models/post.js
+++ models/post.js
@@ -1,5 +1,23 @@
 const mongoose=require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    
-})
+    content :
+    {
+        type:String,
+        required:true
+
+    },
+    // the post to be linked to the user , so we will store that in user field
+    user:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+    }
+},{
+    timestamps:true
+});
+
+
+const Post=mongoose.model('Post',postSchema);
+
+module.exports=Post;
