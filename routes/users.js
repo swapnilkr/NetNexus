fba83f4ec566f510d05e5bd0ec74003d128436d8
Swapnil <swapnilkr099@gmail.com>
2020-12-15 19:03:26 +0530

    Added Update Profile Form

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,8 @@ const usersController=require('../controllers/users_controller');
 // get for links
 router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
 
+router.post('/update/:id',passport.checkAuthentication,usersController.update);
+
 router.get('/sign-up',usersController.signUp);
 
 router.get('/sign-in',usersController.signIn);
