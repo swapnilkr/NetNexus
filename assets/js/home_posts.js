e3dc939347abf438c615d8e2b22dd1ac289f2f44
Swapnil <swapnilkr099@gmail.com>
2020-12-20 20:17:55 +0530

    Added ajx

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,8 +11,9 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
+                   console.log(data);
                    let newPost = newPostDom(data.data.post);
-                   $(`#posts-list-conatiner>ul`).prepend(newPost);
+                    $('#posts-list-conatiner>ul').prepend(newPost);
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -22,43 +23,38 @@
 
 
     // method to create a post in DOM
-    
-    let newPostDom = function(post){
-        return $(`<li id="post-${ post._id }">
-        <p>
-            <small>
-                <a class="delete-post-button" href="/posts/destroy/${  post.id }">
-                    X
-                </a>
-            </small>
-            <% } %>
-            ${post.content}
-            <br>
-            <small>
-                ${post.user.name}
-            </small>
-        </p>
-        <div class="post-comments">
-            
-                <form action="/comments/create"  method="POST">
+     // method to create a post in DOM
+     let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+                    <p>
+                        
+                        <small>
+                            <a class="delete-post-button"  href="/posts/destroy/${ post.id }">X</a>
+                        </small>
+                       
+                        ${ post.content }
+                        <br>
+                        <small>
+                        ${ post.user.name }
+                        </small>
+                    </p>
+                    <div class="post-comments">
+                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                                <input type="hidden" name="post" value="${ post._id }" >
+                                <input type="submit" value="Add Comment">
+                            </form>
+               
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${ post._id }">
+                                
+                            </ul>
+                        </div>
+                    </div>
                     
-                    <input type="text" name="content" placeholder="Type Here to add comment..." required>
-                    
-                    <!-- hidden input for which post comment is made -->
-    
-                    <input type="hidden" name="post" value="${ post._id }">
-                    <input type="submit" value="Add Comment">
-                </form>
-            
-    
-            <div class="post-comments-list">
-                <ul id="post-comments-${ post._id }">
-                    
-                </ul>
-            </div>   
-    
-        </div>
-    </li>`)
+                </li>`)
     }
 
     createPost();
